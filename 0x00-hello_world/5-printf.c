fc90d664f2782a70643d98003cff94aa26aa8325
osunx <splendorfreeman1@gmail.com>
2023-06-15 12:26:16 -0700

    Low level Programming Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - An entry into a C program
  * with proper grammar, but the outcome is a piece of art,: Return of main
  * void - Indicate the program will not take input from user
- * description: A program Using the puts function
+ * description: A program Using the printf function
  * Return: (0); (success) toindicate the code ran without any errors
 */
 int main(void)
