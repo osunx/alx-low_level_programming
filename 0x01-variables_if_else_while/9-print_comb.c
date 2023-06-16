cab8014e3b7db117a6d28074fd85914a3fa1de28
osunx <splendorfreeman1@gmail.com>
2023-06-16 15:25:00 -0700

    Low level programming in C if...else

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,7 +6,21 @@
  * Description: Prints all possible combinations of single-digit numbers
  * separated by ", " in ascending order
  * using the putchar function
- *
+ * Alternative
+ * int main(void)
+ * {
+ * int n;
+ * for (n = 0; n <= 9; n++)
+ * {
+ * putchar ((n % 10) + '0');
+ * if (n == 9)
+ * continue;
+ * putchar(',');
+ * putchar(' ');
+ * }
+ * putchar('\n');
+ * return (0);
+ * }
  * Return: 0 (Success)
  */
 int main(void)
@@ -15,8 +29,11 @@ int pCombo;
 for (pCombo = 0; pCombo <= 9; pCombo++)
 {
 putchar(pCombo + '0');
+if (pCombo != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
