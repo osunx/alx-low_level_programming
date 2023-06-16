8f06075f55b8c7434bacbdece2036904fcf6521b
osunx <splendorfreeman1@gmail.com>
2023-06-16 10:44:15 -0700

    Low level programmin in C if...else statemant

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -14,18 +15,17 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("The number %d is ", n);
 if (n > 0)
 {
-Printf("positive\n");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("zero\n");
+printf("%d is zero\n", n);
 }
 else
-}
-printf("negative\n");
+{
+printf("%d is negative\n", n);
 }
 return (0);
 }
