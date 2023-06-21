93dd8f3747178f1dfccdee51f356d5adb05b014e
osunx <splendorfreeman1@gmail.com>
2023-06-20 19:51:51 -0700

    C Functions, nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,24 +6,39 @@
  *
  * Return: number of digits
 */
+#include <stdio.h>
+
 int main(void)
 {
-unsigned long long fib1 = 1, fib2 = 2, nextTerm;
-int count;
+int count, initial0s;
+unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-printf("%llu, %llu", fib1, fib2);
-
-for (count = 3; count <= 98; ++count)
+for (count = 1; count <= 98; ++count)
 {
-nextTerm = fib1 + fib2;
+if (f1o > 0)
+printf("%lu", f1o);
+initial0s = numLength(mx) - 1 - numLength(f1);
 
-printf(", %llu", nextTerm);
-
-fib1 = fib2;
-fib2 = nextTerm;
+while (f1o > 0 && initial0s > 0)
+{
+printf("%d", 0);
+--initial0s;
 }
 
+printf("%lu", f1);
+
+sum = (f1 + f2) % mx;
+sumo = f1o + f2o + (f1 + f2) / mx;
+f1 = f2;
+f1o = f2o;
+f2 = sum;
+f2o = sumo;
+
+if (count != 98)
+printf(", ");
+else
 printf("\n");
+}
 
 return (0);
 }
