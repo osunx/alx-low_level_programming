1c8dac8ec2be7b491386c7dbdda6fcaefea808a0
osunx <splendorfreeman1@gmail.com>
2023-06-20 20:18:28 -0700

    C Functions, nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,17 +3,21 @@
 /**
  * main - Entry into a C program
  *
+ * printFibonacci - Prints the Fibonacci sequence up to the specified number of terms.
+ *
+ * @n: The number of Fibonacci terms to print.
  *
  * Return: number of digits
 */
-int main(void)
+
+void printFibonacci(int n)
 {
 unsigned int fib1 = 1, fib2 = 2, nextTerm;
 int count;
 
 printf("%u, %u", fib1, fib2);
 
-for (count = 3; count <= 98; ++count)
+for (count = 3; count <= n; ++count)
 {
 nextTerm = fib1 + fib2;
 
@@ -22,7 +26,11 @@ printf(", %u", nextTerm);
 fib1 = fib2;
 fib2 = nextTerm;
 }
+}
 
+int main(void)
+{
+printFibonacci(98);
 printf("\n");
 
 return (0);
