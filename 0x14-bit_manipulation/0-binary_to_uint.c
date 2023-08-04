f1c6142b5be90e5dc2a0055991035c83155db75c
osunx <splendorfreeman1@gmail.com>
2023-08-04 14:35:22 -0700

    BitWise operation and Bit manipulation in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,78 @@
 #include "main.h"
 
 /**
-* binary_to_uint - Converts a binary number to an unsigned int.
-*
-* @b: Pointer to a string of 0 and 1 characters.
-*
-* Return: The converted number, or 0 if:
-* - The input string @b is NULL.
-* - There is one or more characters in the string @b that is not 0 or 1.
+ * _atoi - converts chars to integer
+ *
+ * @c: char to convert
+ *
+ * Return: converted integer
+*/
+unsigned int _atoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+
+/**
+ * _strlen - gets the length of a string
+ *
+ * @str: string input
+ *
+ * Return: string length
+*/
+unsigned int _strlen(const char *str)
+{
+	unsigned int index = 0;
+
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
+
+/**
+ * binary_to_uint - a function that converts a binary number
+ *                  to an unsigned int
+ *
+ * @b: string that contains 0 and 1 characters
+ *
+ * Return: the number converted or 0
+ *         if @b contains a character
+ *         that is not 0 or 1 or when
+ *         @b is null
 */
 unsigned int binary_to_uint(const char *b)
 {
-/* Check if the input string is NULL */
-if (b == NULL)
-return (0);
+	int index;
+	unsigned int result = 0, base2 = 1,  num = 0;
 
-unsigned int result = 0;
+	/*if b is NULL return 0*/
+	if (b == NULL)
+		return (0);
 
-/* Iterate through the characters of the string */
-while (*b != '\0')
-{
-/* Check if the character is a valid binary digit (either '0' or '1') */
-if (*b != '0' && *b != '1')
-return (0);
+	#ifdef DEBUG /*print debug statement*/
+	printf("String is %s, and length is %u.\n", b, _strlen(b));
+	#endif
 
-/* Convert the binary digit to an unsigned int and update the result */
-result = (result << 1) + (*b - '0');
-b++;
-}
+	/*iterate through string*/
+	for (index = _strlen(b) - 1; index >= 0; index--)
+	{
+		num = _atoi(b[index]); /*convert char to number*/
 
-/* Return the converted unsigned int */
-return (result);
+		#ifdef DEBUG /*print debug statements*/
+		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
+		#endif
+
+		/*if number is not 0 or 1 return 0*/
+		if (num != 0 && num != 1)
+			return (0);
+
+		result += num * base2; /*enable debug to see it in action*/
+		base2 *= 2;
+
+		#ifdef DEBUG /*print debug statements*/
+		printf("Result is %u.\n", result);
+		#endif
+	}
+
+	return (result);
+
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,56 @@
 #include "main.h"
 
+#define CHAR_BITS 8 /*size of char*/
+#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
+
 /**
-* set_bit - Sets the value of a bit to 1 at a given index.
-*
-* @n: Pointer to the number containing the bit.
-* @index: The index of the bit to set, starting from 0.
-*
-* Return: 1 if it worked, or -1 if an error occurred.
+ * print_bin - prints binary rep of @n
+ *
+ * @number: decimal value
+ *
+ * Return: nothing
+*/
+void print_bin(unsigned long int number)
+{
+	if (number >> 1)
+		print_bin(number >> 1);
+
+	putc((number & 1) ? '1' : '0', stdout);
+}
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given
+ *           index.
+ *
+ * @n: number
+ * @index: index to set the value of bit to 1
+ *
+ * Return: 1 if it worked OR -1 if it failed
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(unsigned long int) * 8))
-return (-1);
+	unsigned long int mask = 1;
+	#ifdef DEBUG
+	unsigned long int n_before = *n;
+	#endif
 
-unsigned long int mask = 1UL << index;
-*n = *n | mask;
+	/*if index is greater than size of n in binary coded decimal*/
+	if (index > INT_BITS)
+		return (-1);
 
-return (1);
+	mask <<= index; /*create mask based on index position*/
+
+	*n = (*n | mask);
+
+	#ifdef DEBUG
+	printf("\n%ld in binary is ", n_before);
+	print_bin(n_before);
+	printf(" while mask right shifted to index %d is ", index);
+	print_bin(mask);
+	printf(" our new value n in binary is ");
+	print_bin(*n);
+	printf("\n\n");
+	#endif
+
+	return (1);
 }
