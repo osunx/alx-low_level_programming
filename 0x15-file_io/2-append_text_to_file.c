1b94afbd1101024c5abac4241c7093d2cefef7d8
osunx <splendorfreeman1@gmail.com>
2023-08-07 19:03:08 -0700

    C file READ only WRITE only and RDRW

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * append_text_to_file - Appends text to the end of a file.
  * @filename: The name of the file.
@@ -9,34 +8,33 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fdescriptor, contentwritten, wordslen = 0;
+int filedescriptor, contentwritten, wordslen = 0;
 
 /* Check if filename is NULL and return -1 */
 if (filename == NULL)
 return (-1);
 
-/*Open the file in write-only mode with O_WRONLY and O_APPEND flags.*/
-/* O_APPEND ensures that data is added to the end of the file. */
-fdescriptor = open(filename, O_WRONLY | O_APPEND);
-if (fdescriptor == -1)
+/* Open the file in write-only mode with O_WRONLY and O_APPEND flags. */
+filedescriptor = open(filename, O_WRONLY | O_APPEND);
+if (filedescriptor == -1)
 return (-1);
 
-/*If text_content is not NULL, calculate the length of*/
-/*the string and append the content to the file */
+/*If text_content is not NULL,*/
+/*calculate the length of the string and append the content to the file */
 if (text_content != NULL)
 {
 while (text_content[wordslen] != '\0')
 wordslen++;
-contentwritten = write(fescriptor, text_content, wordslen);
+contentwritten = write(filedescriptor, text_content, wordslen);
 /* Check if write was successful */
 if (contentwritten != wordslen)
 {
-close(fdescriptor);
+close(filedescriptor);
 return (-1);
 }
 }
 
 /* Close the file and return 1 to indicate success */
-close(fdescriptor);
+close(filedescriptor);
 return (1);
 }
