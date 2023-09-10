8ed1881d12986a7cc3b44eea652270c91103ba08
osunx <splendorfreeman1@gmail.com>
2023-09-10 14:14:08 -0700

    some changes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
+int _putchar(char c);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
